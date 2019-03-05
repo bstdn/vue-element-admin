223fd597d5fe330b5ae7e9d8fbe979da628165a1
何秀钢 <xiugang.he@chukou1.com>
2019-03-05 10:35:30 +0800

    svg图标

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -58,8 +58,17 @@ module.exports = {
         ]
       },
       {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/icons')],
+        options: {
+          symbolId: 'icon-[name]'
+        }
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
+        exclude: [resolve('src/icons')],
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
