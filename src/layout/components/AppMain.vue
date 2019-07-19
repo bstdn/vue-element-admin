a18185e1bdff3614a3cf61be40553a5a1bc13914
何秀钢 <xiugang.he@chukou1.com>
2019-07-19 19:34:13 +0800

    主样式

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,11 +1,54 @@
 <template>
-  <div>
-    <router-view />
-  </div>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="key">
+        <router-view :key="key" />
+      </keep-alive>
+    </transition>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'AppMain'
+  name: 'AppMain',
+  computed: {
+    key() {
+      return this.$route.fullPath
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .app-main {
+    /* 50= navbar  50  */
+    min-height: calc(100vh - 50px);
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .fixed-header+.app-main {
+    padding-top: 50px;
+  }
+
+  .hasTagsView {
+    .app-main {
+      /* 84 = navbar + tags-view = 50 + 34 */
+      min-height: calc(100vh - 84px);
+    }
+
+    .fixed-header+.app-main {
+      padding-top: 84px;
+    }
+  }
+</style>
+
+<style lang="scss">
+  // fix css style bug in open el-dialog
+  .el-popup-parent--hidden {
+    .fixed-header {
+      padding-right: 15px;
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+
+import 'normalize.css/normalize.css'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
