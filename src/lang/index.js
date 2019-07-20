5ab60b8ff4bd3aa4586a18e5a46eaf8021903f13
何秀钢 <xiugang.he@chukou1.com>
2019-07-20 09:33:30 +0800

    语言包

---
+++ src/lang/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import Cookies from 'js-cookie'
+import elementEnLocale from 'element-ui/lib/locale/lang/en'
+import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import enLocale from './en'
+import zhLocale from './zh'
+
+Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    ...enLocale,
+    ...elementEnLocale
+  },
+  zh: {
+    ...zhLocale,
+    ...elementZhLocale
+  }
+}
+
+export function getLanguage() {
+  const chooseLanguage = Cookies.get('language')
+  if (chooseLanguage) return chooseLanguage
+
+  // if has not choose language
+  const language = (navigator.language || navigator.browserLanguage).toLowerCase()
+  const locales = Object.keys(messages)
+  for (const locale of locales) {
+    if (language.indexOf(locale) > -1) {
+      return locale
+    }
+  }
+  return 'en'
+}
+
+const i18n = new VueI18n({
+  locale: getLanguage(),
+  messages
+})
+
+export default i18n
--- src/main.js
+++ src/main.js
@@ -11,12 +11,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.use(ElementUI)
+import i18n from './lang'
+
+Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
